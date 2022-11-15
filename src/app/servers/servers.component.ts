80065fdb2d2909a50eeea8d5a26eb59599405a5c
adam-p <adam.pietk@gmail.com>
2022-11-15 21:50:02 +0100

    yes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: 'app-servers', // general selector
-  // selector: '[app-servers]',  // selector as attribute
+  // selector: '[app-servers]',  // selector as attribute .
   selector: '.app-servers',     // selector as class .
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
