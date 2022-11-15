d8ae6fbed2c9c2bbad188c2061ecd445d713b8f9
adam-p <adam.pietk@gmail.com>
2022-11-15 21:12:34 +0100

    capture 2 selector udem trening angula my dream apps

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-servers', // general selector
+  // selector: '[app-servers]',  // selector as attribute
+  selector: '.app-servers',     // selector as class
+  template: '<app-server></app-server><app-server></app-server>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
